ae841b5229365b0cd66be7745bc9e1ff321d7753
Matthew Brignola <matthewbrignola@ymail.com>
2022-03-29 14:49:37 -0600

    still not working, trying to keep my snity

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,19 +1,79 @@
 import { gql } from "@apollo/client";
 
-export const CREATE_USER = gql`
-  mutation createUser($username: String!, $email: String!, $password: String!) {
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        savedBooks {
+          bookId
+          title
+          description
+          authors
+          link
+          image
+        }
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
     createUser(username: $username, email: $email, password: $password) {
       token
       user {
         _id
         username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          title
+          description
+          authors
+          image
+          link
+        }
       }
     }
   }
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook($bookData: bookInput) {
-    saveBook(bookData: $bookData)
+  mutation saveBook($input: bookInput!) {
+    saveBook(input: $input: ) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
   }
+`;
+
+export const REMOVE_BOOK = gql`
+mutation removeBook(bookId: $bookId) {
+  _id
+  username
+  email
+  bookCount
+  savedBooks {
+    bookId
+    authors
+    image
+    description
+    title
+    link
+  }
+}
 `;
