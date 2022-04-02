16f95e57a2639fd9c9326a9f919c299ca7858d4b
Matthew Brignola <matthewbrignola@ymail.com>
2022-04-01 19:35:14 -0600

    sign up back end bug fixed

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -28,9 +28,7 @@ const SignupForm = () => {
     event.preventDefault();
 
     try {
-      alert("test");
       const { data } = await addUser({ variables: { ...userFormData } });
-      alert(data);
       Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
